f6a0dcba758bd69a672e63b8841c019696fad7c4
Levi Wheatcroft <levi@wheatcrofts.com.au>
2017-05-24 18:14:15 +0800

    switched to metalsmith-cache dependency

--- test/fixtures/build/test.md
+++
@@ -1,4 +0,0 @@
-
-[Metalsmith][1] is the static site generator that slays dragons and stuff. 
-
-[1]: https://metalsmith.io "metalsmith site"
--- test/index.js
+++ test/index.js
@@ -1,26 +1,21 @@
 import plugin from '../lib'
-import nock from 'nock'
+import {
+  back as nockBack
+} from 'nock'
 import config from 'config'
 import debug from 'debug'
 import { join } from 'path'
 import { assert } from 'chai'
 import Metalsmith from 'metalsmith'
 import sinon from 'sinon'
+import hjson from 'hjson'
 import { writeFileSync, readFileSync } from 'fs'
 
 const dbg = debug('metalsmith-google-drive')
+nockBack.setMode('record')
+nockBack.fixtures = 'test/fixtures/scrape'
 
-nock.recorder.rec()
-function getFix (src) {
-  let buffer = readFileSync(join('test/fixtures', src))
-  return JSON.parse(buffer.toString('utf8'))
-}
-
-function setFix (value, src) {
-  writeFileSync(join('test/fixtures', src), JSON.stringify(value))
-}
-
-describe('metalsmith-google-drive', () => {
+describe('metalsmith-google-drive test', () => {
   beforeEach(() => {
     // create spy
     // sinon.spy(cloudinary.api, 'resources')
@@ -29,15 +24,19 @@ describe('metalsmith-google-drive', () => {
     // cloudinary.api.resources.restore()
   })
   it('should be able to scrape a folder', (done) => {
-    Metalsmith('test/fixtures')
-    .use(plugin({
-      auth: config.get('metalsmith-google-drive'),
-      src: '0B1QpLgu4mpt8YXJTYzRlZURkazg',
-      dest: 'articles'
-    }))
-    .build((err, files) => {
-      if (err) return done(err)
-      done()
+    nockBack('nock', (writeRequests) => {
+      Metalsmith('test/fixtures/scrape')
+      .use(plugin({
+        auth: config.get('metalsmith-google-drive'),
+        src: '0B1QpLgu4mpt8YXJTYzRlZURkazg',
+        dest: 'articles',
+        invalidateCache: true
+      }))
+      .build((err, files) => {
+        if (err) return done(err)
+        // writeRequests()
+        done()
+      })
     })
   }).timeout(0)
 })
